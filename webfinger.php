4ad61bd91de39be247f7d391be163bb4c9cb2d88
Kreeblah <kreeblah@gmail.com>
2023-09-04 17:33:51 -0700

    Initial commit

---
+++ webfinger.php
@@ -0,0 +1,9 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/jrd+json');
+if($_GET["rel"]=="http://openid.net/specs/connect/1.0/issuer" && preg_match("/@example\.com$/i", $_GET["resource"]) && preg_match("/^acct\:/", $_GET["resource"])) {
+  echo "{\"subject\":\"" . $_GET["resource"] . "\",\"links\":[{\"rel\":\"http://openid.net/specs/connect/1.0/issuer\",\"href\":\"https://auth.example.com\"}]}";
+} else {
+  echo "{}";
+}
+?>
