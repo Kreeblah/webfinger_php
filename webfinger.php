985e2dd83d7fcab28ee358b1ca3dcb3fd527fb4f
Kreeblah <kreeblah@gmail.com>
2023-09-04 20:40:16 -0700

    Remove rel parameter requirement

--- webfinger.php
+++ webfinger.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/jrd+json');
-if($_GET["rel"]=="http://openid.net/specs/connect/1.0/issuer" && preg_match("/@example\.com$/i", $_GET["resource"]) && preg_match("/^acct\:/", $_GET["resource"])) {
+if(preg_match("/@example\.com$/i", $_GET["resource"]) && preg_match("/^acct\:/", $_GET["resource"])) {
   echo "{\"subject\":\"" . $_GET["resource"] . "\",\"links\":[{\"rel\":\"http://openid.net/specs/connect/1.0/issuer\",\"href\":\"https://auth.example.com\"}]}";
 } else {
   http_response_code(404);
